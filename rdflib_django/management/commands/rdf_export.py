be3629a70ace0022286f36783ff9e364b26b973f
alex <devkral@web.de>
2020-05-27 20:02:21 +0200

    fix #5, port commands to new django version

--- rdflib_django/management/commands/rdf_export.py
+++ rdflib_django/management/commands/rdf_export.py
@@ -1,7 +1,6 @@
 """
 Management command for exporting RDF from the store.
 """
-from optparse import make_option
 from django.core.management.base import BaseCommand
 import sys
 from rdflib.term import URIRef
@@ -13,22 +12,6 @@ class Command(BaseCommand):
     Command object for exporting RDF.
     """
 
-    option_list = BaseCommand.option_list + (
-        make_option(
-            '--context', '-c', type='string', dest='context',
-            help='Only RDF data from the context with this identifier will be exported. If not specified, a new blank context is created.'  # noqa: E501
-        ),
-
-        make_option('--store', '-s', type='string', dest='store',
-            help='RDF data will be exported from the store with this identifier. If not specified, the default store is used.'  # noqa: E501
-        ),
-
-        make_option(
-            '--format', '-f', type='string', dest='format', default='xml',
-            help='Format of the RDF data. This option accepts all formats allowed by rdflib. Defaults to xml.'  # noqa: E501
-        )
-    )
-
     help = """Exports an RDF resource.
 
 Examples:
@@ -36,7 +19,22 @@ Examples:
     {0} rdf_export --format n3 my_file.n3
     {0} rdf_export --context http://example.com/context
     """.format(sys.argv[0])
-    args = 'file'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--context', '-c', dest='context',
+            help='Only RDF data from the context with this identifier will be exported. If not specified, a new blank context is created.'  # noqa: E501
+        )
+
+        parser.add_argument(
+            '--store', '-s', dest='store',
+            help='RDF data will be exported from the store with this identifier. If not specified, the default store is used.'  # noqa: E501
+        )
+
+        parser.add_argument(
+            '--format', '-f', dest='format', default='xml',
+            help='Format of the RDF data. This option accepts all formats allowed by rdflib. Defaults to xml.'  # noqa: E501
+        )
 
     def handle(self, *args, **options):
         store_id = options.get('store')
--- rdflib_django/management/commands/rdf_import.py
+++ rdflib_django/management/commands/rdf_import.py
@@ -1,7 +1,6 @@
 """
 Management command for parsing RDF into the store.
 """
-from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 import sys
 from django.db import transaction
@@ -15,23 +14,6 @@ class Command(BaseCommand):
     Command object for importing RDF.
     """
 
-    option_list = BaseCommand.option_list + (
-        make_option(
-            '--store', '-s', type='string', dest='store',
-            help='RDF data will be imported into the store with this identifier. If not specified, the default store is used.'  # noqa: E501
-        ),
-
-        make_option(
-            '--context', '-c', type='string', dest='context',
-            help='RDF data will be imported into a context with this identifier. If not specified, a new blank context is created.'  # noqa: E501
-        ),
-
-        make_option(
-            '--format', '-f', type='string', dest='format', default='xml',
-            help='Format of the RDF data. This option accepts all formats allowed by rdflib. Defaults to xml.'  # noqa: E501
-        )
-    )
-
     help = """Imports an RDF resource.
 
 Examples:
@@ -39,9 +21,24 @@ Examples:
     {0} rdf_import --format n3 my_file.n3
     {0} rdf_import --context http://zoowizard.eu http://zoowizard.eu/datasource/zoochat/294
     """.format(sys.argv[0])  # noqa: E501
-    args = 'file-or-resource'
 
-    @transaction.commit_on_success
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--store', '-s', dest='store',
+            help='RDF data will be imported into the store with this identifier. If not specified, the default store is used.'  # noqa: E501
+        )
+
+        parser.add_argument(
+            '--context', '-c', dest='context',
+            help='RDF data will be imported into a context with this identifier. If not specified, a new blank context is created.'  # noqa: E501
+        )
+
+        parser.add_argument(
+            '--format', '-f', dest='format', default='xml',
+            help='Format of the RDF data. This option accepts all formats allowed by rdflib. Defaults to xml.'  # noqa: E501
+        )
+
+    @transaction.atomic
     def handle(self, *args, **options):
         if not args:
             raise CommandError("No file or resource specified.")
